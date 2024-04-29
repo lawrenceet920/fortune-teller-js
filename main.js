7b67097d54c719d7d9e9c4b10662a54a8653ac8d
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-04-29 14:30:33 -0400

    made main code

---
+++ main.js
@@ -0,0 +1,18 @@
+// Ethan Lawrence
+// April 29 2024
+// fortune-teller-js
+
+// Imput
+const jobs = ['Farmer', 'Cobbler', 'Court Jester', 'Advisor', 'Blacksmith', 'Landlord', 'Merchant', 'Web Developer'];
+const contries = ['Scotland', 'England', 'Ireland', 'Wales', 'United Kingdom', 'Canada'];
+
+// process
+const randContry = contries[Math.floor(Math.random() * contries.length)];
+const randJob = jobs[Math.floor(Math.random() * jobs.length)];
+
+const firstName = prompt('Enter your firstname');
+
+const fortune = `In ${Math.floor(Math.random() * 10)} years, ${firstName} will be a(n) ${randJob}, in ${randContry}`;
+
+// Output
+console.log (fortune);
