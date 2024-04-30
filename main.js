7432cc331740e435c27793ef883748f596f7c47d
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-04-30 13:45:24 -0400

    finished visuals

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 
 // Imput
 const jobs = ['Farmer', 'Cobbler', 'Court Jester', 'Advisor', 'Blacksmith', 'Landlord', 'Merchant', 'Web Developer'];
-const contries = ['Scotland', 'England', 'Ireland', 'Wales', 'United Kingdom', 'Canada'];
+const contries = ['Scotland', 'England', 'Ireland', 'Wales', 'The United Kingdom', 'Canada'];
 
 // process
 const randContry = contries[Math.floor(Math.random() * contries.length)];
@@ -12,7 +12,7 @@ const randJob = jobs[Math.floor(Math.random() * jobs.length)];
 
 const firstName = prompt('Enter your firstname');
 
-const fortune = `In ${Math.floor(Math.random() * 10)} years, ${firstName} will be a(n) ${randJob}, in ${randContry}`;
+const fortune = `In ${Math.floor(Math.random() * 9) + 1} years, ${firstName} will be a(n) ${randJob}, in ${randContry}`;
 
 // Output
-console.log (fortune);
+document.getElementById('output').innerHTML = fortune;
